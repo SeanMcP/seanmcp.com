ae1df6f0cc87d42868625d94f9cd70ea8ffa14c1
Sean McPherson <git@seanmcp.com>
2020-05-12 14:43:13 -0400

    feat: change markdown template engine
    Using liquid prevents you from using `{{ double braces }}`. Not good for a React developer.

--- .eleventy.js
+++ .eleventy.js
@@ -107,7 +107,7 @@ module.exports = function(eleventyConfig) {
     // This is only used for URLs (it does not affect your file structure)
     pathPrefix: '/',
 
-    markdownTemplateEngine: 'liquid',
+    markdownTemplateEngine: 'html',
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
     passthroughFileCopy: true,
