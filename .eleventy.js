bf936a35499923e690d07b40dec06f3fd61ac16f
Sean McPherson <git@seanmcp.com>
2020-06-02 20:54:02 -0400

    feat: add js directory to build

--- .eleventy.js
+++ .eleventy.js
@@ -56,6 +56,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('img')
   eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('js')
 
   /* Markdown Plugins */
   const markdownIt = require('markdown-it')
