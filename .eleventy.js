7976e6bd253017131c214d07823bcef18f77a77b
Sean McPherson <git@seanmcp.com>
2021-03-24 14:43:24 -0400

    feat: publish article

--- .eleventy.js
+++ .eleventy.js
@@ -52,6 +52,8 @@ module.exports = function (eleventyConfig) {
         return string.toUpperCase();
       case "javascript":
         return "JavaScript";
+      case "macos":
+        return "macOS";
       case "typescript":
         return "TypeScript";
       default:
