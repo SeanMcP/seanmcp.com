5e81f59225bd7423002c258896d38fff94c0dca5
Sean McPherson <git@seanmcp.com>
2022-07-20 11:28:41 -0400

    fix: remove stray commas from noteList

--- src/_11ty/noteList.js
+++ src/_11ty/noteList.js
@@ -3,14 +3,16 @@ const note = require("./note");
 module.exports = function noteList(props) {
   return /* html */ `
 <section class="note-list">
-  ${props.notes.map((item) =>
-    note({
-      content: item.templateContent,
-      date: item.date,
-      edited: item.data.edited,
-      url: item.url,
-    })
-  )}
+  ${props.notes
+    .map((item) =>
+      note({
+        content: item.templateContent,
+        date: item.date,
+        edited: item.data.edited,
+        url: item.url,
+      })
+    )
+    .join("")}
 </section>
     `;
 };
