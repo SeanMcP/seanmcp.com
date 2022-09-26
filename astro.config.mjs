01dfc2389cbb9f3e1ab001fc243f3c259651815c
Sean McPherson <git@seanmcp.com>
2022-09-26 11:02:32 -0400

    feat: add /sam

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+
 import metadata from "./src/data/metadata.json";
 
 // https://astro.build/config
 export default defineConfig({
   site: metadata.url,
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte()],
 });
