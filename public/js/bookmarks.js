5b8ae4eea9a0e0cf0b0b00367ab388af09dbbeca
Sean McPherson <git@seanmcp.com>
2022-10-29 14:42:57 -0400

    fix: use fresh data on save/remove

--- public/js/bookmarks.js
+++ public/js/bookmarks.js
@@ -2,13 +2,21 @@
 
 (() => {
   const storageKey = "com.seanmcp.bookmarks";
-  const bookmarks = JSON.parse(localStorage.getItem(storageKey) || "[]");
 
-  const indicator = document.createElement("span");
-  indicator.role = "img";
-  indicator.setAttribute("aria-label", "bookmarked");
-  indicator.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><path d="M184,32H72A16,16,0,0,0,56,48V224a8.1,8.1,0,0,0,4.1,7,7.6,7.6,0,0,0,3.9,1,7.9,7.9,0,0,0,4.2-1.2L128,193.4l59.7,37.4a8.3,8.3,0,0,0,8.2.2,8.1,8.1,0,0,0,4.1-7V48A16,16,0,0,0,184,32Z"></path></svg>`;
-  indicator.title = "Previously bookmarked content";
+  function getBookmarks() {
+    return JSON.parse(localStorage.getItem(storageKey) || "[]");
+  }
+
+  const bookmarks = getBookmarks();
+
+  function getIndicator() {
+    const indicator = document.createElement("span");
+    indicator.role = "img";
+    indicator.setAttribute("aria-label", "bookmarked");
+    indicator.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="192" height="192" fill="#000000" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><path d="M184,32H72A16,16,0,0,0,56,48V224a8.1,8.1,0,0,0,4.1,7,7.6,7.6,0,0,0,3.9,1,7.9,7.9,0,0,0,4.2-1.2L128,193.4l59.7,37.4a8.3,8.3,0,0,0,8.2.2,8.1,8.1,0,0,0,4.1-7V48A16,16,0,0,0,184,32Z"></path></svg>`;
+    indicator.title = "Previously bookmarked content";
+    return indicator;
+  }
 
   document.querySelectorAll("[data-url]").forEach((node) => {
     if (bookmarks.includes(node.dataset.url)) {
@@ -16,7 +24,7 @@
 
       // Notes render their own bookmark toggle, so we don't need an icon
       if (!node.dataset.url.includes("/notes/")) {
-        node.appendChild(indicator.cloneNode(true));
+        node.appendChild(getIndicator());
       }
     }
   });
@@ -46,7 +54,7 @@
       toggle.dataset.bookmarked = "true";
       localStorage.setItem(
         storageKey,
-        JSON.stringify([...bookmarks, toggle.dataset.url])
+        JSON.stringify([...getBookmarks(), toggle.dataset.url])
       );
       // Wait for browser to paint before alerting
       setTimeout(() => alert("Bookmarking this content for later"), 0);
@@ -54,7 +62,9 @@
       toggle.dataset.bookmarked = "false";
       localStorage.setItem(
         storageKey,
-        JSON.stringify(bookmarks.filter((url) => url !== toggle.dataset.url))
+        JSON.stringify(
+          getBookmarks().filter((url) => url !== toggle.dataset.url)
+        )
       );
     }
   });
