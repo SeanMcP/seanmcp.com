87054539d2322bcb3353f769ffdb37f1a2c491f4
Sean McPherson <sean@seanmcp.com>
2023-05-18 08:28:48 -0400

    read less

--- netlify/edge-functions/random-article.ts
+++ netlify/edge-functions/random-article.ts
@@ -1,12 +1,36 @@
+import { readLines } from "https://deno.land/std@0.177.0/io/read_lines.ts";
+
 const markdownFiles = Deno.readDirSync("src/content/articles");
 
 const paths = []
 
 for (const file of markdownFiles) {
-    // Look into reading only the first few lines of the file
-    const contents = Deno.readTextFileSync(`src/content/articles/${file.name}`)
+    if (!file.isFile) continue
+
+    const reader = await Deno.open(`src/content/articles/${file.name}`)
     
-    if (!file.isFile || contents.includes("- DRAFT")) continue
+    let frontmatterOpen = false;
+    let isDraft = false;
+
+    for await (const line of readLines(reader)) {
+        if (line === "---") {
+            if (frontmatterOpen) {
+                // frontmatter already open, so this is the closing line
+                break;
+            } else {
+                frontmatterOpen = true
+            }
+        } else {
+            if (line.includes("- DRAFT")) {
+                isDraft = true
+                break;
+            }
+        }
+    }
+
+    reader.close()
+    
+    if (isDraft) continue
 
     const slug = file.name.slice(0, file.name.lastIndexOf("."))
     paths.push(`/articles/${slug}`)
