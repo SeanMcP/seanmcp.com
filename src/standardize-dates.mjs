229d83d6e48288743e19813aebd0591f8050c7c6
Sean McPherson <sean@seanmcp.com>
2023-08-05 09:29:59 -0400

    ready to write

--- src/standardize-dates.mjs
+++ src/standardize-dates.mjs
@@ -1,87 +1,74 @@
-import fs from "node:fs"
-import path from "node:path"
+import fs from "node:fs";
+import path from "node:path";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
-const dirPath = path.join(__dirname, "./content/articles")
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const dirPath = path.join(__dirname, "./content/articles");
 
-const files = fs.readdirSync(dirPath)
+const files = fs.readdirSync(dirPath);
 
 function forceTwoDigits(number) {
-    const string = String(number)
+  const string = String(number);
 
-    if (string.length === 1) {
-        return "0" + string
-    }
-    return string
+  if (string.length === 1) {
+    return "0" + string;
+  }
+  return string;
 }
 
 /**
- * @param {Date} date 
- * @returns 
+ * @param {Date} date
+ * @returns
  */
 function getTimestamp(date) {
-    const dateParts = [
-        date.getFullYear(),
-        date.getMonth() + 1,
-        date.getDate(),
-    ].map(forceTwoDigits)
-    const timeParts = [
-        date.getHours(),
-        date.getMinutes()
-    ].map(forceTwoDigits)
+  const dateParts = [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+  ].map(forceTwoDigits);
+  const timeParts = [date.getHours(), date.getMinutes()].map(forceTwoDigits);
 
-    return `${dateParts.join('-')}T${timeParts.join(':')}-0400`
+  return `${dateParts.join("-")}T${timeParts.join(":")}-0400`;
 }
 
-files.forEach((file) => {
-    const contents = fs.readFileSync(path.join(dirPath, file), "utf8")
-    const key = "pubDate: "
-    const anchor = contents.indexOf(key) + key.length
-    const chunk = contents.slice(anchor, anchor + 36)
-    const dateString = chunk.slice(0, chunk.indexOf('\n'))
-    // The above is neat and all, but I should probably be using String.replace
-    // with a regex pattern
-    contents.replace(/pubDate: (.*)\n/, (_, pubDate) => {
-        const date = new Date(pubDate)
+/**
+ *
+ * @param {Date} date
+ */
+function valiDate(date) {
+  if (date.getDay() === 0) {
+    console.log("Uh oh! That is a Sunday");
+    throw Error(date.toISOString());
+  }
+}
 
-        // console.log(pubDate)
-
-        if (!pubDate.includes("T")) {
-            // It's one of the date-only timestamps
-            date.setHours(12)
-            date.setUTCDate(date.getUTCDate() + 1)
-            // console.log([pubDate, getTimestamp(date)])
-            return ""
-        }
+files
+  .filter((file) => file.slice(0, 3) !== "000")
+  .forEach((file) => {
+    const contents = fs.readFileSync(path.join(dirPath, file), "utf8");
     
-        if (pubDate.includes("Z")) {
-            // UTC time!
-            // console.log([pubDate, getTimestamp(date)])
-            return ""
-        }
+    const nextContent = contents.replace(/pubDate: (.*)\n/, (_, pubDate) => {
+      const date = new Date(pubDate);
 
-        // console.count('OTHER')
-        let next = pubDate.slice(0, pubDate.lastIndexOf('.') - 3)
-        next += "-0400"
-        // console.log([pubDate, next])
-        return ""
-    })
+      if (!pubDate.includes("T")) {
+        // It's one of the date-only timestamps
+        date.setHours(12);
+        date.setUTCDate(date.getUTCDate() + 1);
+        valiDate(date);
+        return `pubDate: ${getTimestamp(date)}\n`;
+      }
 
-    if (!dateString.includes("T")) {
-        // It's one of the date-only timestamps. Add a publish time at noon.
-    }
+      if (pubDate.includes("Z")) {
+        // UTC time!
+        valiDate(date);
+        return `pubDate: ${getTimestamp(date)}\n`;
+      }
 
-    if (dateString.includes("Z")) {
-        // UTC time! Subtract four hours and set the correct timezone.
-    }
-    
-    const date = new Date(dateString)
+      // The few that already have a timezone
+      let nextPubDate = pubDate.slice(0, pubDate.lastIndexOf(".") - 3);
+      nextPubDate += "-0400";
+      valiDate(new Date(nextPubDate));
+      return `pubDate: ${nextPubDate}\n`;
+    });
 
-    // I don't think this will actually be an issue once I resolve all of the T-
-    // less dates, but I'll keep this check for now.
-    if (date.getDay() === 0) {
-        // console.log('!!! uh oh, it is a Sunday !!!')
-        // console.log(dateString)
-        // console.log(contents.slice(4, contents.indexOf("description:")))
-    }
-})
+    fs.writeFileSync(path.join(dirPath, file), nextContent)
+  });
