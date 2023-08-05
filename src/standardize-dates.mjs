e70c8614efb31355e67b042db3e1e7fe257db450
Sean McPherson <sean@seanmcp.com>
2023-08-04 22:28:21 -0400

    add comments

--- src/standardize-dates.mjs
+++ src/standardize-dates.mjs
@@ -12,9 +12,21 @@ files.forEach((file) => {
     const anchor = contents.indexOf(key) + key.length
     const chunk = contents.slice(anchor, anchor + 36)
     const dateString = chunk.slice(0, chunk.indexOf('\n'))
+    // The above is neat and all, but I should probably be using String.replace
+    // with a regex pattern
+
+    if (!dateString.includes("T")) {
+        // It's one of the date-only timestamps. Add a publish time at noon.
+    }
+
+    if (dateString.includes("Z")) {
+        // UTC time! Subtract four hours and set the correct timezone.
+    }
     
     const date = new Date(dateString)
 
+    // I don't think this will actually be an issue once I resolve all of the T-
+    // less dates, but I'll keep this check for now.
     if (date.getDay() === 0) {
         console.log('!!! uh oh, it is a Sunday !!!')
         console.log(dateString)
